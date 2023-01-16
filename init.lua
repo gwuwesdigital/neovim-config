e0893934a0554bc965cb95288b28340ebb964d01
George Wu <gwu@wesfarmers.com.au>
2023-01-16 13:00:08 +1100

    generalise telescope file search
    * use git_files in a git repo and find_files otherwise

--- init.lua
+++ init.lua
@@ -15,5 +15,9 @@ require("user.treesitter")
 require("user.variables")
 require("user.whichkey")
 
+-- Allow require to look in after/plugin folder
+local home_dir = os.getenv("HOME")
+package.path = home_dir .. "/.config/nvim/after/plugin/?.lua;" .. package.path
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
