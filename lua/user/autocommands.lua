b16ffaf33ccabdf9f92d7740845be678771c8cfb
George Wu <gwu@wesfarmers.com.au>
2023-01-17 14:53:11 +1100

    update autocommands
    * move fugitive specific keybind to under _git augroup
    * check filetype instead of using pattern

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -33,6 +33,30 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	group = _git,
 	pattern = { "gitcommit" },
 })
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+	callback = function()
+		if vim.bo.ft ~= "fugitive" then
+			return
+		end
+
+		local bufnr = vim.api.nvim_get_current_buf()
+
+		vim.keymap.set("n", "<leader>f", function()
+			vim.cmd.Git("fetch")
+		end, { buffer = bufnr, remap = false, desc = "Fetch" })
+
+		vim.keymap.set("n", "<leader>P", function()
+			vim.cmd.Git("push")
+		end, { buffer = bufnr, remap = false, desc = "Push" })
+
+		-- rebase always
+		vim.keymap.set("n", "<leader>p", function()
+			vim.cmd.Git("pull --rebase")
+		end, { buffer = bufnr, remap = false, desc = "Pull" })
+	end,
+	group = _git,
+	pattern = "*",
+})
 
 local _markdown = vim.api.nvim_create_augroup("Markdown", { clear = true })
 vim.api.nvim_create_autocmd({ "FileType" }, {
@@ -60,29 +84,12 @@ vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGai
 	pattern = { "*" },
 })
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	command = "call fugitive#ReloadStatus()",
-	group = _auto_reload,
-	pattern = { "fugitive://**" },
-})
-
-local _fugitive = vim.api.nvim_create_augroup("FugitiveExtraKeys", { clear = true })
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 	callback = function()
 		if vim.bo.ft ~= "fugitive" then
 			return
 		end
-
-		local bufnr = vim.api.nvim_get_current_buf()
-
-		vim.keymap.set("n", "<leader>P", function()
-			vim.cmd.Git("push")
-		end, { buffer = bufnr, remap = false, desc = "Push" })
-
-		-- rebase always
-		vim.keymap.set("n", "<leader>p", function()
-			vim.cmd.Git("pull --rebase")
-		end, { buffer = bufnr, remap = false, desc = "Pull" })
+        vim.cmd[[call fugitive#ReloadStatus()]]
 	end,
-	group = _fugitive,
-	pattern = "*",
+	group = _auto_reload,
+	pattern = { "*" },
 })
