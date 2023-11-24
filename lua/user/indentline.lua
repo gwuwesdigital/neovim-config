337999ce5ba0128445a04da6559d1f8542a0415f
George Wu <gwu@wesdigital.com.au>
2023-11-24 17:24:23 +1100

    upgrade indentline syntax to v3

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -1,4 +1,4 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, indent_blankline = pcall(require, "ibl")
 if not status_ok then
 	return
 end
@@ -6,40 +6,13 @@ end
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
 indent_blankline.setup({
-	show_current_context = true,
-	char = "▏",
-	show_trailing_blankline_indent = false,
-	show_first_indent_level = true,
-	use_treesitter = true,
-	buftype_exclude = { "terminal", "nofile" },
-	filetype_exclude = {
-		"help",
-		"startify",
-		"packer",
-		"neogitstatus",
-		"NvimTree",
-		"Trouble",
-	},
-	context_patterns = {
-		"class",
-		"return",
-		"function",
-		"method",
-		"^if",
-		"^while",
-		"jsx_element",
-		"^for",
-		"^object",
-		"^table",
-		"block",
-		"arguments",
-		"if_statement",
-		"else_clause",
-		"jsx_element",
-		"jsx_self_closing_element",
-		"try_statement",
-		"catch_clause",
-		"import_statement",
-		"operation_type",
+	indent = { char = "▏" },
+	-- show_first_indent_level = true,
+	-- use_treesitter = true,
+	-- buftype_exclude = { "terminal", "nofile" },
+	exclude = {
+		filetypes = {
+			"undotree",
+		},
 	},
 })
