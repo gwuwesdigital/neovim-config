ce9de2d28344f7d73fffd37e5b246304b0a5e02b
George Wu <gwu@wesdigital.com.au>
2024-07-08 14:02:52 +1000

    update deprecated syntax

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -7,12 +7,12 @@ end
 -- See `:help gitsigns.txt`
 gitsigns.setup({
 	signs = {
-		add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-		change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-		delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-		untracked = { hl = "GitSignsAdd", text = "┆", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+		add = { text = "│" },
+		change = { text = "│" },
+		delete = { text = "_" },
+		topdelete = { text = "‾" },
+		changedelete = { text = "~" },
+		untracked = { text = "┆" },
 	},
 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -30,10 +30,6 @@ gitsigns.setup({
 		delay = 1000,
 		ignore_whitespace = false,
 	},
-	-- current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-	current_line_blame_formatter_opts = {
-		relative_time = true,
-	},
 	sign_priority = 6,
 	update_debounce = 100,
 	status_formatter = nil, -- Use default
@@ -45,8 +41,5 @@ gitsigns.setup({
 		relative = "cursor",
 		row = 0,
 		col = 1,
-	},
-	yadm = {
-		enable = false,
 	},
 })
