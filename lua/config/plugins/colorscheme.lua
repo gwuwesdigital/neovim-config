47e87c8cca0473a8467f3a50732bfdeca13a198a
George Wu <gwu@wesdigital.com.au>
2026-06-10 08:44:00 +1000

    (style) revert back to tokyonight defaults

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -11,31 +11,31 @@ return {
 				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
 				sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
 
-				-- Change the "hint" color to the "orange" color, and make the "error" color bright red
-				on_colors = function(colors)
-					colors.comment = "#737aa2" --colors.dark5
-					colors.fg_gutter = "#545c7e" --colors.dark3
-					colors.git = {
-						change = "#86e1fc", --colors.cyan
-						add = "#c3e88d", --colors.green
-						delete = "#ff757f", --colors.red
-					}
-				end,
+				-- -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+				-- on_colors = function(colors)
+				-- 	colors.comment = "#737aa2" --colors.dark5
+				-- 	colors.fg_gutter = "#545c7e" --colors.dark3
+				-- 	colors.git = {
+				-- 		change = "#86e1fc", --colors.cyan
+				-- 		add = "#c3e88d", --colors.green
+				-- 		delete = "#ff757f", --colors.red
+				-- 	}
+				-- end,
 
-				on_highlights = function(highlights, c)
-					highlights.CursorLine = {
-						bg = "#444a73", --colors.terminal_black
-					}
-					highlights.CursorLineNr = {
-						fg = "#c8d3f5", --colors.fg
-					}
-					highlights.GitSignsCurrentLineBlame = {
-						fg = "#828bb8", --colors.fg_dark
-					}
-					highlights.DiagnosticUnnecessary = {
-						fg = c.comment, --colors.terminal_black
-					}
-				end,
+				-- on_highlights = function(highlights, c)
+				-- 	highlights.CursorLine = {
+				-- 		bg = "#444a73", --colors.terminal_black
+				-- 	}
+				-- 	highlights.CursorLineNr = {
+				-- 		fg = "#c8d3f5", --colors.fg
+				-- 	}
+				-- 	highlights.GitSignsCurrentLineBlame = {
+				-- 		fg = "#828bb8", --colors.fg_dark
+				-- 	}
+				-- 	highlights.DiagnosticUnnecessary = {
+				-- 		fg = c.comment, --colors.terminal_black
+				-- 	}
+				-- end,
 			})
 			-- load the colorscheme here
 			vim.cmd([[colorscheme tokyonight]])
